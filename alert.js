d0cd471eff286a735106d13a6179aee3ea3aa2eb
rasikakannan0510 <rasikakannan0510@gmail.com>
2026-05-01 10:05:22 +0530

    Add files via upload

--- alert.js
+++ alert.js
@@ -1,19 +1,50 @@
+// alert.js
+
 const triggerBtn = document.getElementById("trigger");
 const alertBox = document.getElementById("alertBox");
 const alarm = document.getElementById("alarm");
+const statusText = document.getElementById("status");
+const datetime = document.getElementById("datetime");
+const liveTime = document.getElementById("liveTime");
 
-triggerBtn.addEventListener("click", function(){
+function updateClock(){
+let now = new Date();
+datetime.innerHTML = now.toLocaleString();
+}
+
+setInterval(updateClock,1000);
+updateClock();
+
+triggerBtn.addEventListener("click", async function(){
 
 alertBox.classList.remove("hidden");
 
-alarm.loop = true;
-alarm.play();
+statusText.innerHTML = "Emergency Alert Activated";
 
-if(navigator.vibrate){
-navigator.vibrate([500,200,500,200,500]);
+alarm.loop = true;
+
+try{
+alarm.play().then(() => {
+    console.log("Sound playing");
+}).catch(error => {
+    alert("Click again to enable sound");
+});
+}catch(error){
+alert("Tap again if sound is blocked.");
 }
 
+if(navigator.vibrate){
+navigator.vibrate([500,200,500,200,500,200,500]);
+}
+
+showLiveTime();
+
 });
+
+function showLiveTime(){
+let now = new Date();
+liveTime.innerHTML = "Alert Time: " + now.toLocaleTimeString();
+}
 
 function stopAlert(){
 
@@ -21,5 +52,11 @@ alertBox.classList.add("hidden");
 
 alarm.pause();
 alarm.currentTime = 0;
+
+statusText.innerHTML = "System Ready";
+
+if(navigator.vibrate){
+navigator.vibrate(0);
+}
 
 }
